fcaee9416c5ab89867b90785547ce8219edb172a
hpc <liuyongjie9313@yahoo.co.jp>
2017-06-07 22:45:24 +0900

    change_flag

--- GAN_model.py
+++ GAN_model.py
@@ -350,29 +350,30 @@ class DCGAN(object):
             return False, 0
 
 
-flags = tf.app.flags
-flags.DEFINE_integer("iter", 10000, "iter to train ")
-flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")
-flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
-flags.DEFINE_integer("train_size", np.inf, "The size of train images [np.inf]")
-flags.DEFINE_integer("sample_num", 64, "The size of sample images ")
-flags.DEFINE_integer("batch_size", 64, "The size of batch images [64]")
-flags.DEFINE_integer("input_height", 28, "The size of image to use (will be center cropped). [108]")
-flags.DEFINE_integer("input_width", 28, "The size of image to use (will be center cropped). If None, same value as input_height [None]")
-flags.DEFINE_integer("output_height", 28, "The size of the output images to produce [64]")
-flags.DEFINE_integer("output_width", 28, "The size of the output images to produce. If None, same value as output_height [None]")
-flags.DEFINE_string("dataset", "5", "The name of dataset [...]")
-flags.DEFINE_string("input_fname_pattern", "*.jpg", "Glob pattern of filename of input images [*]")
-flags.DEFINE_string("checkpoint_dir", "./checkpoint", "Directory name to save the checkpoints [checkpoint]")
-flags.DEFINE_string("sample_dir", "./samples", "Directory name to save the image samples [samples]")
-flags.DEFINE_boolean("train", True, "True for training, False for testing [False]")
-flags.DEFINE_boolean("crop", True, "True for training, False for testing [False]")
-flags.DEFINE_boolean("visualize", False, "True for visualizing, False for nothing [False]")
-FLAGS = flags.FLAGS
-
-
 
 def main(_):
+
+
+    flags = tf.app.flags
+    flags.DEFINE_integer("iter", 10000, "iter to train ")
+    flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")
+    flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
+    flags.DEFINE_integer("train_size", np.inf, "The size of train images [np.inf]")
+    flags.DEFINE_integer("sample_num", 64, "The size of sample images ")
+    flags.DEFINE_integer("batch_size", 64, "The size of batch images [64]")
+    flags.DEFINE_integer("input_height", 28, "The size of image to use (will be center cropped). [108]")
+    flags.DEFINE_integer("input_width", 28, "The size of image to use (will be center cropped). If None, same value as input_height [None]")
+    flags.DEFINE_integer("output_height", 28, "The size of the output images to produce [64]")
+    flags.DEFINE_integer("output_width", 28, "The size of the output images to produce. If None, same value as output_height [None]")
+    flags.DEFINE_string("dataset", "5", "The name of dataset [...]")
+    flags.DEFINE_string("input_fname_pattern", "*.jpg", "Glob pattern of filename of input images [*]")
+    flags.DEFINE_string("checkpoint_dir", "./checkpoint", "Directory name to save the checkpoints [checkpoint]")
+    flags.DEFINE_string("sample_dir", "./samples", "Directory name to save the image samples [samples]")
+    flags.DEFINE_boolean("train", True, "True for training, False for testing [False]")
+    flags.DEFINE_boolean("crop", True, "True for training, False for testing [False]")
+    flags.DEFINE_boolean("visualize", False, "True for visualizing, False for nothing [False]")
+    FLAGS = flags.FLAGS
+
 
     if not os.path.exists(FLAGS.checkpoint_dir):
         os.makedirs(FLAGS.checkpoint_dir)
