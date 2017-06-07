710a3284b63e39576578db1d045e53583b5efc93
wangqianlong120 <wangqianlong120@gmail.com>
2017-06-07 21:41:44 +0900

    classification

--- MNIST_Classification.py
+++ MNIST_Classification.py
@@ -1,1 +1,91 @@
 import tensorflow as tf
+import numpy as np
+slim = tf.contrib.slim
+from glob import glob
+import os
+
+
+class Classification_Model(object):
+
+    def __init__(self):
+
+        self.img_size = 28
+        self.feature_size = 100
+        self.end_points = {}
+        self.input_fname_pattern = '*.jpg'
+        self.batch_size = 50
+
+    def transform(self, img):
+        return img/127.5 - 1
+
+    def model_arg_scope(self, weight_decay=0.0005, is_training = True):
+        with slim.arg_scope([slim.conv2d, slim.fully_connected],
+                            activation_fn = tf.nn.relu,
+                            normalizer_fn = slim.batch_norm,
+                            normalizer_params={'is_training': is_training, 'decay': 0.9},
+                            weights_regularizer=slim.l2_regularizer(weight_decay),
+                            weights_initializer=tf.truncated_normal_initializer(stddev=0.01),
+                            biases_initializer=tf.zeros_initializer()
+                            ):
+            with slim.arg_scope([slim.conv2d, slim.max_pool2d], padding = 'SAME') as sc:
+                return sc
+
+    def net(self, inputs, is_training=True, dropout_keep_prob=0.5, reuse=None, scope='AlexNet'):
+
+        with tf.variable_scope(scope, 'AlexNet', [inputs], reuse=reuse):
+            net = slim.conv2d(inputs, 16, [3, 3], scope='conv1')
+            net = slim.max_pool2d(net, [2, 2], scope='pool1')
+            net = slim.conv2d(net, 32, [3, 3], scope='conv2')
+            net = slim.conv2d(net, 64, [3, 3], scope='conv3')
+            net = slim.max_pool2d(net, [2, 2], scope='pool3')
+            net = slim.conv2d(net, 128, [3, 3], scope='conv4')
+            net = slim.conv2d(net, 256, [3, 3], scope='conv5')
+            net = slim.conv2d(net, 512, [3, 3], scope='conv6')
+            net = slim.avg_pool2d(net, [7, 7], stride = 1, scope='average_pool')
+            net = slim.flatten(net)
+            features = slim.fully_connected(net, self.feature_size, scope='features')
+            self.end_points['Features'] = features
+            logits = slim.fully_connected(features, 10, activation_fn = None, scope='logits')
+            self.end_points['Logits'] = logits
+            predictions = tf.nn.softmax(logits, name='Predictions')
+            self.end_points['Predictions'] = predictions
+        return logits, self.end_points
+
+    def losses(self, logits, labels, scope='model_losses'):
+
+        with tf.name_scope(scope, 'model_losses'):
+            losses = tf.nn.softmax_cross_entropy_with_logits(labels=labels,logits=logits)
+            losses = tf.reduce_mean(losses)
+
+        return losses
+
+    def optimizer(self, learning_rate, scope = 'model_optimizer'):
+
+        with tf.name_scope(scope, 'model_optimizer'):
+            optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+
+        return optimizer
+
+    def change_dataset(self, dataset_name = "0"):
+        self.dataset_name = dataset_name
+        self.file_list = glob(os.path.join("./data", self.dataset_name, self.input_fname_pattern))
+        self.filename_queue = tf.train.string_input_producer(self.file_list[:])
+        self.image_reader = tf.WholeFileReader()
+        _, self.image_file = self.image_reader.read(self.filename_queue)
+
+        self.image = tf.image.decode_jpeg(self.image_file)
+
+        self.image = tf.cast(tf.reshape(self.image, shape=[28, 28, 1]), dtype=tf.float32)
+
+    def get_batch(self):
+
+        num_preprocess_threads = 1
+        min_queue_examples = 256
+
+        batch_images = tf.train.batch([self.image],
+                                       batch_size=self.batch_size,
+                                       num_threads=num_preprocess_threads,
+                                       capacity=min_queue_examples + 3 * self.batch_size)
+        batch_images = self.transform(batch_images)
+        batch_labels = tf.one_hot(tf.ones([self.batch_size], dtype=tf.int32) * int(self.dataset_name), depth=10)
+        return batch_images, batch_labels
---
+++ train_classification_new.py
@@ -0,0 +1,124 @@
+import tensorflow as tf
+import numpy as np
+import os
+
+slim = tf.contrib.slim
+
+from MNIST_Classification import Classification_Model
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+def main(_):
+
+    tf.logging.set_verbosity(tf.logging.DEBUG)
+
+    with tf.Graph().as_default():
+
+        #mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+        ckpt = tf.train.get_checkpoint_state(os.path.dirname('./checkpoints/checkpoint'))
+
+        sess = tf.InteractiveSession()
+
+        global_step = slim.create_global_step()
+        summaries = set(tf.get_collection(tf.GraphKeys.SUMMARIES))
+
+        mnist_net = Classification_Model()
+        mnist_net.change_dataset("5")
+        x, y_ = mnist_net.get_batch()
+        #y_ = tf.one_hot(tf.ones([mnist_net.batch_size], dtype=tf.int32) * 5, depth=10)
+
+        arg_scope = mnist_net.model_arg_scope()
+        end_points = {}
+
+        with slim.arg_scope(arg_scope):
+            logits, end_points = mnist_net.net(x)
+            losses = mnist_net.losses(logits, y_)
+            train_step = mnist_net.optimizer(0.001).minimize(losses, global_step=global_step)
+
+        #total_loss = tf.losses.get_total_loss()
+        summaries.add(tf.summary.image("img", tf.cast(x, tf.float32)))
+        summaries.add(tf.summary.scalar('loss', losses))
+
+        for variable in tf.trainable_variables():
+            summaries.add(tf.summary.histogram(variable.op.name, variable))
+        train_writer = tf.summary.FileWriter('./train',
+                                             sess.graph)
+        correct_prediction = tf.equal(tf.argmax(end_points['Predictions'], 1), tf.argmax(y_, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        summaries.add(tf.summary.scalar('accuracy', accuracy))
+
+        summary_op = tf.summary.merge(list(summaries), name='summary_op')
+        sess.run(tf.global_variables_initializer())
+        saver = tf.train.Saver(max_to_keep=5,
+                               keep_checkpoint_every_n_hours=1.0,
+                               write_version=2,
+                               pad_step_number=False)
+
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+        train_num = 0
+        all_features = np.zeros((5000, 100))
+
+        for i in range(100):
+
+            # _, summary_str = sess.run([train_step, summary_op])
+            featurens_str = sess.run([mnist_net.end_points["Features"]])
+            all_features[i*50:i*50+50,:] = featurens_str[0]
+
+            global_step_str = global_step.eval()
+
+            # if global_step_str % 10000 == 0:
+            #     train_num = (train_num + 1) % 10
+            #     mnist_net.change_dataset(str(train_num))
+                #t_x, t_y = mnist_net.get_batch()
+                #tf.assign(x, t_x)
+                #tf.assign(y_, t_y)
+
+            # if global_step_str % 100 == 0:
+            #     train_writer.add_summary(summary_str, global_step_str)
+            #     loss_str = sess.run([losses])
+            #     print ("step %d, loss = %f" % (global_step_str, loss_str[0]))
+            #
+            # if global_step_str % 1000 == 0:
+            #     sum_accuracy = 0.0
+            #     for i in range(10):
+            #         accuracy_str = sess.run([accuracy])
+            #         sum_accuracy += accuracy_str[0]
+            #
+            #     print ("accuracy: %f" % (sum_accuracy / 10.0))
+
+        coord.request_stop()
+        coord.join(threads)
+
+        np.save("./outputs/features_5", all_features)
+
+            # if global_step_str % 1000 == 0:
+            #     sum_accuracy = 0.0
+            #     for i in range(600):
+            #         test_batch = mnist.train.next_batch(100)
+            #         accuracy_str = sess.run([accuracy], feed_dict={x: np.reshape(test_batch[0], (-1, 28, 28, 1)), y_: test_batch[1]})
+            #         sum_accuracy += accuracy_str[0]
+            #
+            #     print ("accuracy: %f" % (sum_accuracy / 600.0))
+            #     saver.save(sess, "./checkpoints/",global_step=global_step_str)
+
+
+        # sum_accuracy = 0.0
+        # for i in range(100):
+        #     test_batch = mnist.test.next_batch(100)
+        #     accuracy_str = sess.run([accuracy], feed_dict={x: np.reshape(test_batch[0], (-1, 28, 28, 1)), y_: test_batch[1]})
+        #     sum_accuracy += accuracy_str[0]
+
+        #print ("accuracy: %f" % (sum_accuracy / 100.0))
+
+if __name__ == '__main__':
+    tf.app.run()
+
+
+
+
