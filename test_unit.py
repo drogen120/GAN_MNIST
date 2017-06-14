8fbeb15e2158542fac46523a76642b2264627725
hpc <liuyongjie9313@yahoo.co.jp>
2017-06-14 16:46:31 +0900

    bug fixed

--- test_unit.py
+++ test_unit.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-z_pool = np.load('./outputs/features_1.npy')
+z_pool = np.load('./outputs/features_2.npy')
 num = z_pool.shape[0]
 random_index = np.round(np.random.uniform(0,1,[1]) * (num-1)).astype(np.int32)
 batch_z = np.zeros([1,100],dtype = np.float32)
