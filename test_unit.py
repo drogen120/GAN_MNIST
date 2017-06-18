cf4f667391e9914f78348a92df993975d92c88c5
hpc <liuyongjie9313@yahoo.co.jp>
2017-06-18 15:40:01 +0900

    .

--- test_unit.py
+++ test_unit.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 
-z_pool = np.load('./outputs/features_2.npy')
+z_pool = np.load('./outputs/features_9.npy')
+print ('the shape of this npy is ', z_pool.shape)
 num = z_pool.shape[0]
 random_index = np.round(np.random.uniform(0,1,[1]) * (num-1)).astype(np.int32)
 batch_z = np.zeros([1,100],dtype = np.float32)
